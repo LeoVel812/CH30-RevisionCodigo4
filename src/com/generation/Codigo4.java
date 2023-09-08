424556f119f729c5e8472b00c5fe6736a19c033b
Leonardo Velazquez <leovelazquez812@gmail.com>
2023-09-08 11:11:21 -0600

    game is fixed and commented

--- src/com/generation/Codigo4.java
+++ src/com/generation/Codigo4.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 /*
  * Programa sobre el juego de piedra, papel o tijeras 
+ * Los Dos jugadores insertan entradas de texto
+ * para ver quien es ganador primero se revisa si son iguales, sino 
+ * se asume que el jugador 2 es el ganador y con un switch se entra a la opcion 
+ * elegida por el jugador 1 y se compara si tal comparacion la gana el jugador 1,
+ * si es asi se redefine el jugador uno.
  */
 public class Codigo4 {
 	// agregar metodo main
@@ -21,26 +26,26 @@ public class Codigo4 {
 		if (j1.equals(j2)) {// quitar ) del if, y cambiar == al metodo equals
 			System.out.println("Empate");
 		} else {
-			int g = 2;
+			int ganador = 2; // renombrar g a ganador
 			switch (j1) {
 			case "piedra":
 				if (j2.equals("tijeras")) {// cambiar == por el metodo equals para comparar el contenido
-					g = 1;
+					ganador = 1;
 				}
 			case "papel":
 				if (j2.equals("piedra")) {// cambiar == por el metodo equals
-					g = 1;
+					ganador = 1;
 				} // agregar llave de cierre del if
-			case "tijeras"://agregar s a tijera
+			case "tijeras":// agregar s a tijera
 				if (j2.equals("papel")) {
-					g = 1;
+					ganador = 1;
 				}
 				break;
 			default:
 			}
-			System.out.println("Gana el jugador " + g);
+			System.out.println("Gana el jugador " + ganador);
 		}
-		s.close();
+		s.close();// agregar cierre de objeto Scanner
 	}
 
 }
